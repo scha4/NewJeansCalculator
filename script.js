692ec7c32060e9b6e06fac57d9211e1c7cdee86f
“Sam <scha4@gmu.edu>
2023-03-13 15:56:24 +0900

    changed styling

---
+++ script.js
@@ -0,0 +1,16 @@
+const add = function (a, b) {
+  return a + b;
+};
+const sub = function (a, b) {
+  return a - b;
+};
+
+const divide = function (a, b) {
+  return a / b;
+};
+
+const multi = function (a, b) {
+  return a * b;
+};
+
+console.log(add(2, 9, 9));
